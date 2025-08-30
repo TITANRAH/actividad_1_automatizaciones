32a977c95ae059f3793792c7a6226c46d5b26ee8
TITANRAH <granrah1@gmail.com>
2025-08-29 20:14:11 -0400

    new division function

--- test/lib/maths.spec.ts
+++ test/lib/maths.spec.ts
@@ -1,15 +1,19 @@
-import { suma, resta, multiplicacion } from "@/libs/maths";
+import { suma, resta, multiplicacion, division } from "@/libs/maths";
 
 describe("Maths", () => {
-  test("Deberia sumar 2 + 3 y dar 5", () => {
+  it("Deberia sumar 2 + 3 y dar 5", () => {
     expect(suma(2, 3)).toBe(5);
   });
 
-  test("Deberia restar 5 - 3 y dar 2", () => {
+  it("Deberia restar 5 - 3 y dar 2", () => {
     expect(resta(5, 3)).toBe(2);
   });
 
-  test("Deberia multiplicar 2 * 3 y dar 6", () => {
+  it("Deberia multiplicar 2 * 3 y dar 6", () => {
     expect(multiplicacion(2, 3)).toBe(6);
+  });
+
+  it("Deberia dividir 6 / 3 y dar 2", () => {
+    expect(division(6, 3)).toBe(2);
   });
 });
